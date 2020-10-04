fcc479a9d3004867da70fe5f84085a0994613810
JONNAGADDALA RAVIKUMAR <ravikumarjit583@gmail.com>
2020-10-04 21:44:48 +0530

    Added part time employee

--- Employee_Wages.java
+++ Employee_Wages.java
@@ -1,12 +1,15 @@
 package pac3;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class Employee_Wages {
 	public static void main(String[] args) 
 	{
 		int wageHour = 20;
-		int dayHours = 8;
+		int fullHours = 8;
+		int partHours = 4;
+		
 		
 		System.out.println("\n\n\t\t\tWelcome to Employee Wage Computation Program");
 		
@@ -14,13 +17,23 @@ public class Employee_Wages {
 		
 		int randValue = random.nextInt(10);
 		
-		if(randValue <= 6)
-		{
-			System.out.println("Employee is present");
-			System.out.println("Wage : "+(wageHour*dayHours));
-		}	
-		else
-			System.out.println("Employee is absent");
+		
+			if(randValue <= 6)
+			{
+				System.out.println("\n\tFull time Employee is present");
+				System.out.println("\n\tWage : "+(wageHour*fullHours));
+			}
+			else
+				System.out.println("\n\tEmployee is absent");
+		
+		
+			if(randValue <= 6)
+			{
+				System.out.println("\n\tPart time Employee is present");
+				System.out.println("\n\tWage : "+(wageHour*partHours));
+			}
+			else
+				System.out.println("\n\tEmployee is absent");
 	}
 
 }
