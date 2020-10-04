6a5e8ede4ecedd60ff1642a22be12c6171e7d9df
JONNAGADDALA RAVIKUMAR <ravikumarjit583@gmail.com>
2020-10-04 21:40:12 +0530

    Employee present or not

--- Employee_Wages.java
+++ Employee_Wages.java
@@ -1,9 +1,20 @@
 package pac3;
 
+import java.util.Random;
+
 public class Employee_Wages {
 	public static void main(String[] args) 
 	{
 		System.out.println("\n\n\t\t\tWelcome to Employee Wage Computation Program");
+		
+		Random random = new Random();
+		
+		int randValue = random.nextInt(10);
+		
+		if(randValue <= 6)
+			System.out.println("Employee is present");
+		else
+			System.out.println("Employee is absent");
 	}
 
 }
