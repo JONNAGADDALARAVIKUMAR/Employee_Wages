00fa32b665deab44f0283437daa5d0687f6c8a28
JONNAGADDALA RAVIKUMAR <ravikumarjit583@gmail.com>
2020-10-04 22:16:27 +0530

    Condition of hours or days reached

--- Employee_Wages.java
+++ Employee_Wages.java
@@ -3,51 +3,99 @@ package pac3;
 import java.util.Random;
 import java.util.Scanner;
 
+
+
 public class Employee_Wages {
+	static int hrs=0,present=0,max=0,totalPay;
 	public static void main(String[] args) 
 	{
-		int wageHour = 20;
-		int fullHours = 8;
-		int partHours = 4,present=0,absent=0,wage=0;
-		
-		Scanner scan = new Scanner(System.in);
-		
-		System.out.print("\n\n\t\t\tWelcome to Employee Wage Computation Program\n\n\tChoose type of Employee\n\n\t\t1.Full Time Employee\n\t\t2.Part Time Employee\n\tOPTION : ");
-		
-		Random random = new Random();
+		Scanner s = new Scanner(System.in);
 		
 		
 		
-		int empType = scan.nextInt();
+		System.out.print("\tChoose type of Employee\n\t\t1.Full Time\n\t\t2.Part Time\n\tEnter Choice : ");
+		int type = s.nextInt();
+		System.out.println("\n\n\t\t\tWelcome to Employee Wage Computation programme");
 		
-		for(int i=0;i<20;i++)
+		switch(type)
 		{
-			int randValue = random.nextInt(10);
-			switch (empType)
+		case 1:
+			fullTime();
+			break;
+	
+		case 2:
+			parttime();
+			break;
+		}
+		if(hrs>max)
+		{
+			totalPay=max*20;
+		}
+		else
+		{
+			totalPay=hrs*20;
+		}
+		System.out.println("\t\t\t\t\t\t\t\t\t      --------\n\t\t\t\t\t\t\t\t\tTotal\t: "+totalPay+"\n\n\n\t\tNo. of Working days\t: 20 \n\n\t\tNo. of days Worked\t: "+present+"\n\n\t\tNo. of Absents\t\t: "+(20-present)+"\n\n\t\ttotal Working Hours\t: "+hrs+"\n\n\t\tMaximum Working Hours\t: "+max);
+		System.out.println("\n\t\tTotal wage\t\t: "+totalPay);
+	}
+
+	private static void fullTime() 
+	{
+		Random random = new Random();
+		max=100;
+		System.out.println("\n\n\t\tDay\tAttendence\tHours Worked\tDaily Wage\t    Total Wage");
+		for(int i=1;i<=20;i++)
+		{
+			int randValue = random.nextInt(9);
+			if(randValue <= 4)
 			{
-		
-			case 1:
-				if(randValue <= 6)
+				if(hrs<100)
 				{
 					present++;
-					wage+=(wageHour*fullHours);
+					hrs+=8;
+					System.out.println("\t\t "+i+"\t    PR\t\t    "+hrs+"\t\t   160.00\t\t"+(hrs*20));
 				}
-				else
-					absent++;;
-				break;
-			case 2:
-			
-				if(randValue <= 6)
+				else if(hrs>=100)
 				{
-					present++;
-					wage+=(wageHour*partHours);
+					System.out.println("\t\t "+i+"\t    AB\t\t    "+hrs+"\t\t   0.00\t\t\t"+(hrs*20));
 				}
-				else
-					absent++;;
-				break;
+			}
+			else
+			{
+				System.out.println("\t\t "+i+"\t    AB\t\t    "+hrs+"\t\t   0.00\t\t\t"+(hrs*20));
+			}
+		}		
+	}
+
+	private static void parttime() 
+	{
+		Random random = new Random();
+		max=50;
+		System.out.println("\n\n\t\tDay\tAttendence\tHours Worked\tDaily Wage\t   Total Wage");
+		for(int i=1;i<=20;i++)
+		{
+			int randValue = random.nextInt(9);
+			if(randValue <= 4)
+			{
+				if(hrs<50)
+				{
+					
+					present++;
+					hrs+=4;
+					System.out.println("\t\t "+i+"\t    PR\t\t    "+hrs+"\t\t   80.00\t\t"+(hrs*20));
+				}
+				else if(hrs >=50)
+				{
+					System.out.println("\t\t "+i+"\t    AB\t\t    "+hrs+"\t\t   0.00\t\t\t"+(hrs*20));
+				}
+					
+			}
+			else
+			{
+				System.out.println("\t\t "+i+"\t    AB\t\t    "+hrs+"\t\t   0.00\t\t\t"+(hrs*20));
 			}
 		}
-		System.out.println("\nPresent : "+present+"\nAbsent : "+absent+"\ntotal Wage for a month : "+wage);
+		
 	}
 
 }
