6d90597421e41d61a0d9d956595291af0de98ae3
JONNAGADDALA RAVIKUMAR <ravikumarjit583@gmail.com>
2020-10-04 21:36:43 +0530

    Welcome Message

---
+++ Employee_Wages.java
@@ -0,0 +1,9 @@
+package pac3;
+
+public class Employee_Wages {
+	public static void main(String[] args) 
+	{
+		System.out.println("\n\n\t\t\tWelcome to Employee Wage Computation Program");
+	}
+
+}
