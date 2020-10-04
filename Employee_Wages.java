e9c1cd248bacbf229bfd23ea82ed1bf4a3c0aa0e
JONNAGADDALA RAVIKUMAR <ravikumarjit583@gmail.com>
2020-10-04 21:42:18 +0530

    Wage per day

--- Employee_Wages.java
+++ Employee_Wages.java
@@ -5,6 +5,9 @@ import java.util.Random;
 public class Employee_Wages {
 	public static void main(String[] args) 
 	{
+		int wageHour = 20;
+		int dayHours = 8;
+		
 		System.out.println("\n\n\t\t\tWelcome to Employee Wage Computation Program");
 		
 		Random random = new Random();
@@ -12,7 +15,10 @@ public class Employee_Wages {
 		int randValue = random.nextInt(10);
 		
 		if(randValue <= 6)
+		{
 			System.out.println("Employee is present");
+			System.out.println("Wage : "+(wageHour*dayHours));
+		}	
 		else
 			System.out.println("Employee is absent");
 	}
